687ed62041ea5759f312f89798acefec72c2e5f4
Carter <carterm126@gmail.com>
2019-05-31 20:12:01 -0500

    Add create user function to write user data to db on signup

--- src/create-user.js
+++ src/create-user.js
@@ -1,6 +1,25 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context) {
-  console.log(event, context);
+export async function main(event, context, callback) {
+  const { region, userName, request } = event;
+  // Combine user region and id to match userId in upload table
+  const userId = `${region}:${request.userAttributes.sub}`;
+
+  const params = {
+    TableName: process.env.userTableName,
+    Item: {
+      userId,
+      userName,
+      joinedOn: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, event);
+  } catch (e) {
+    console.error(e);
+    callback(null, event);
+  }
 }
