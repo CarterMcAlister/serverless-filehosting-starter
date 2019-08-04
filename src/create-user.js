52919f992dc5a82620b3dbbccb1d42c8c2252cef
Carter <carterm126@gmail.com>
2019-08-04 18:46:01 -0500

    Update all dependencies to latest versions, add prettier config

--- src/create-user.js
+++ src/create-user.js
@@ -1,10 +1,10 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from './libs/dynamodb-lib'
+import { success, failure } from './libs/response-lib'
 
 export async function main(event, context, callback) {
-  const { region, userName, request } = event;
+  const { region, userName, request } = event
   // Combine user region and id to match userId in upload table
-  const userId = `${region}:${request.userAttributes.sub}`;
+  const userId = `${region}:${request.userAttributes.sub}`
 
   const params = {
     TableName: process.env.userTableName,
@@ -12,15 +12,15 @@ export async function main(event, context, callback) {
       userName,
       userId,
       avatar: null,
-      joinedOn: Date.now(),
-    },
-  };
+      joinedOn: Date.now()
+    }
+  }
 
   try {
-    await dynamoDbLib.call("put", params);
-    callback(null, event);
+    await dynamoDbLib.call('put', params)
+    callback(null, event)
   } catch (e) {
-    console.error(e);
-    callback(null, event);
+    console.error(e)
+    callback(null, event)
   }
 }
